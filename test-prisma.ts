1e292fd624f5a3c0416f94ca5e74b797a94590c9
Anshaq <anshaqmuhammed55@gmail.com>
2026-08-14 22:23:00 +0530

    Fix import path for prisma in test-prisma.ts

--- test-prisma.ts
+++ test-prisma.ts
@@ -1,5 +1,5 @@
 import "dotenv/config";
-import { prisma } from "./src/lib/prisma.ts";
+import { prisma } from "./src/lib/prisma";
 async function run() {
   console.log("Orgs:", await prisma.organization.findMany());
   console.log("Users:", await prisma.user.findMany());
